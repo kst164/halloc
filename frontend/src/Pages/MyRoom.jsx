0142837fed2a06d240218f7622701cb4979241a1
Rahuboy <rahulramach3000@gmail.com>
2022-06-26 18:22:25 +0530

    Made the buttons of MyRoom work

--- frontend/src/Pages/MyRoom.jsx
+++ frontend/src/Pages/MyRoom.jsx
@@ -15,28 +15,77 @@ class MyRoom extends Component {
     otherName: null,
     otherRoll: null,
     applicants: [
-      { name: "Rahul", roll: "cs21btech11047" },
+      { name: "Rahul", roll: "cs21btech11049" },
       { name: "Kushagra", roll: "cs21btech11033" },
       { name: "Kartheek", roll: "cs21btech11028" },
       { name: "Rutv", roll: "ma21btech11014" },
     ],
   };
 
+  onClick = (namestring, rollstring) => {
+    const otherName = namestring;
+    const otherRoll = rollstring;
+    this.setState({
+      otherName: otherName,
+      otherRoll: otherRoll,
+      applicants: [],
+      occupants: 2
+    })
+    console.log(this.state.otherName);
+  }
+
   //Render passes on room number, room occupant details to Card, applicants to Table
   render() {
-    return (
-      <div className="home">
+
+    if(this.state.occupants === 0){
+      return(
+        <div className="home">
         <NavBar />
         <div className="main">
-          <RoomCard
-            roomNumber={this.state.roomNumber}
-            masterName={this.state.masterName}
-            masterRoll={this.state.masterRoll}
-          />
-          <RoomTable applicants={this.state.applicants} />
+          <h1>You haven't selected a room yet</h1>
         </div>
-      </div>
-    );
+        </div>
+      );
+    }
+    else if(this.state.occupants === 1){
+      return (
+        <div className="home">
+          <NavBar />
+          <div className="main">
+            <RoomCard
+              roomNumber={this.state.roomNumber}
+              masterName={this.state.masterName}
+              masterRoll={this.state.masterRoll}
+              otherName={this.state.otherName}
+              otherRoll={this.state.otherRoll}
+              occupants={this.state.occupants}
+            />
+            <RoomTable 
+            applicants={this.state.applicants}
+            onClick = {(namestring, rollstring) => this.onClick(namestring, rollstring)}
+            />
+          </div>
+        </div>
+      );
+    }
+    else{
+      return (
+        <div className="home">
+          <NavBar />
+          <div className="main">
+            <RoomCard
+              roomNumber={this.state.roomNumber}
+              masterName={this.state.masterName}
+              masterRoll={this.state.masterRoll}
+              otherName={this.state.otherName}
+              otherRoll={this.state.otherRoll}
+              occupants={this.state.occupants}
+            />
+          </div>
+        </div>
+      );
+    }
+    
   }
 }
 

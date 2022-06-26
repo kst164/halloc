79de9dfa7ee79e157f89d0aa229caa5d4415cc6d
purplehand52 <drishit2003@gmail.com>
2022-06-26 17:19:38 +0530

    bleh App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-import Grid from './Pages/Grid';
-import HomeGen from './Pages/HomeGen';
-import Login from './Pages/Login';
-import HomeLog from './Pages/HomeLog';
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import MyRoom from './Pages/MyRoom';
+
+
 class App extends Component {
     state = {}
 
     render() {
         return (
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/grid' element={<Grid/>}/>
-                    <Route path='/login' element={<Login/>}/>
-                    <Route path='/' element={<HomeGen/>}/>
-                    <Route path='/loggedin' element={<HomeLog/>}/>
-                </Routes>
-            </BrowserRouter>
+            <MyRoom />
         );
     }
 }

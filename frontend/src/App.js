4beec9258699a5be70a6a97714099480588e8c32
Rahuboy <rahulramach3000@gmail.com>
2022-06-26 19:09:39 +0530

    trying git-ing for the 1000th time

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+// import Grid from './Pages/Grid';
+// import HomeGen from './Pages/HomeGen';
+// import Login from './Pages/Login';
+// import HomeLog from './Pages/HomeLog';
+// import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import MyRoom from './Pages/MyRoom';
-
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 class App extends Component {
     state = {}
 
     render() {
         return (
-            <MyRoom />
+            // <BrowserRouter>
+            //     <Routes>
+            //         <Route path='/grid' element={<Grid/>}/>
+            //         <Route path='/login' element={<Login/>}/>
+            //         <Route path='/' element={<HomeGen/>}/>
+            //         <Route path='/loggedin' element={<HomeLog/>}/>
+            //     </Routes>
+            // </BrowserRouter>
+            <BrowserRouter>
+              <Routes>
+                <Route path='/' element={<MyRoom />} />
+              </Routes>
+            </BrowserRouter>
         );
     }
 }
